6dde6c966a1b05020456a39b25cf1ea886b07921
Justin Kenney <justinkenney13@gmail.com>
2018-11-03 19:34:51 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -36,15 +36,15 @@ end
 
 def can_get_the_first_item_in_the_database
   # first_movie = Movie.find(1)
-  Movie.all.first
+  Movie.first
 end
 
 def can_get_the_last_item_in_the_database
-  Movie.all.last
+  Movie.last
 end
 
 def can_get_size_of_the_database
-  Movie.all.size
+  Movie.size
 end
 
 def can_find_the_first_item_from_the_database_using_id
